b3a67defeb126cbbd567e37e40dc93ab5775b0ff
Jan <jan280808@gmail.com>
2024-12-29 18:37:16 +0100

    Canceled some events

---
+++ src/main/java/de/jan/rpg/core/event/CanceledEvent.java
@@ -0,0 +1,32 @@
+package de.jan.rpg.core.event;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.weather.WeatherChangeEvent;
+
+public class CanceledEvent implements Listener {
+
+    @EventHandler
+    public void onFood(FoodLevelChangeEvent event) {
+        if(event.getFoodLevel() >= 20){
+            event.setCancelled(true);
+            return;
+        }
+        event.setFoodLevel(20);
+    }
+
+    @EventHandler
+    public void onWeather(WeatherChangeEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void spawningEntity(CreatureSpawnEvent event) {
+        CreatureSpawnEvent.SpawnReason spawnReason = event.getSpawnReason();
+        if(spawnReason.equals(CreatureSpawnEvent.SpawnReason.CUSTOM) || spawnReason.equals(CreatureSpawnEvent.SpawnReason.COMMAND)) return;
+        event.setCancelled(true);
+    }
+
+}
